1a8238da17b54c10b81c9cfe5b862162cee6c11a
deathaxe <deathaxe82@gmail.com>
2026-08-13 11:19:03 +0200

    Display quick panel as loading indicator

--- package_control/commands/install_package_command.py
+++ package_control/commands/install_package_command.py
@@ -1,19 +1,44 @@
 import sublime
 import sublime_aio
+import sublime_plugin
 
 from ..activity_indicator import ActivityIndicator
 from ..console_write import console_write
 from ..package_tasks import PackageTaskRunner
 from ..show_error import show_message
 
+STATE_LOADING = 1
+STATE_DISPLAY = 2
+STATE_CANCELLED = 3
 
-class InstallPackageCommand(sublime_aio.ApplicationCommand):
+
+class InstallPackageCommand(sublime_plugin.ApplicationCommand):
     """
     A command that presents the list of available packages and allows the
     user to pick one to install.
+
+    Note: A synchronous command is used to display a "Loading..." quick panel
+    immediatly without any UI flickering, while loading packages in asyncio
+    event loop.
     """
 
-    async def run(self):
+    def run(self):
+        self.state = STATE_LOADING
+        window = sublime.active_window()
+        fut = sublime_aio.run_coroutine(self.arun(sublime_aio.Window(window.id())))
+
+        def on_select(picked):
+            """
+            Cancel execution of arun(), when quick panel is dismissed before
+            loading finished.
+            """
+            if self.state == STATE_LOADING and picked == -1:
+                self.state = STATE_CANCELLED
+                fut.cancel()
+
+        window.show_quick_panel(items=["Loading packages..."], on_select=on_select)
+
+    async def arun(self, window: sublime_aio.Window):
         installer = PackageTaskRunner()
 
         with ActivityIndicator("Loading packages...") as progress:
@@ -21,6 +46,7 @@ class InstallPackageCommand(sublime_aio.ApplicationCommand):
                 actions=(installer.INSTALL, installer.OVERWRITE)
             )
             if not tasks:
+                window.run_command("hide_overlay")
                 message = "There are no packages available for installation"
                 console_write(message)
                 progress.finish(message)
@@ -36,9 +62,17 @@ class InstallPackageCommand(sublime_aio.ApplicationCommand):
 
         items = installer.render_quick_panel_items(tasks)
 
-        picked = await sublime_aio.active_window().show_quick_panel(items, sublime.KEEP_OPEN_ON_FOCUS_LOST)
-        if picked > -1:
-            task = tasks[picked]
+        if self.state == STATE_CANCELLED:
+            return
 
-            with ActivityIndicator("Installing package {}".format(task.package_name)) as progress:
-                await installer.run_install_tasks([task], progress)
+        self.state = STATE_DISPLAY
+        window.run_command("hide_overlay")
+
+        picked = await window.show_quick_panel(items, sublime.KEEP_OPEN_ON_FOCUS_LOST)
+        if picked < 0:
+            return
+
+        task = tasks[picked]
+
+        with ActivityIndicator("Installing package {}".format(task.package_name)) as progress:
+            await installer.run_install_tasks([task], progress)
