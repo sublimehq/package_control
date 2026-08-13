e15c38bd83e70efa432a6b347f2b093ebfe3e099
deathaxe <deathaxe82@gmail.com>
2026-08-13 11:19:03 +0200

    Display quick panel as loading indicator

--- package_control/commands/upgrade_package_command.py
+++ package_control/commands/upgrade_package_command.py
@@ -1,18 +1,43 @@
 import sublime
 import sublime_aio
+import sublime_plugin
 
 from ..activity_indicator import ActivityIndicator
 from ..console_write import console_write
 from ..package_tasks import PackageTaskRunner
 from ..show_error import show_message
 
+STATE_LOADING = 1
+STATE_DISPLAY = 2
+STATE_CANCELLED = 3
 
-class UpgradePackageCommand(sublime_aio.ApplicationCommand):
+
+class UpgradePackageCommand(sublime_plugin.ApplicationCommand):
     """
     A command that presents the list of installed packages that can be upgraded
+
+    Note: A synchronous command is used to display a "Loading..." quick panel
+    immediatly without any UI flickering, while loading packages in asyncio
+    event loop.
     """
 
-    async def run(self):
+    def run(self):
+        self.state = STATE_LOADING
+        window = sublime.active_window()
+        fut = sublime_aio.run_coroutine(self.arun(sublime_aio.Window(window.id())))
+
+        def on_select(picked):
+            """
+            Cancel execution of arun(), when quick panel is dismissed before
+            loading finished.
+            """
+            if self.state == STATE_LOADING and picked == -1:
+                self.state = STATE_CANCELLED
+                fut.cancel()
+
+        window.show_quick_panel(items=["Searching updates..."], on_select=on_select)
+
+    async def arun(self, window: sublime_aio.Window):
         upgrader = PackageTaskRunner()
 
         with ActivityIndicator("Searching updates...") as progress:
@@ -21,6 +46,7 @@ class UpgradePackageCommand(sublime_aio.ApplicationCommand):
                 ignore_packages=upgrader.ignored_packages(),  # don't upgrade disabled packages
             )
             if tasks is False:
+                window.run_command("hide_overlay")
                 message = "There are no packages available for upgrade"
                 console_write(message)
                 progress.finish(message)
@@ -35,6 +61,7 @@ class UpgradePackageCommand(sublime_aio.ApplicationCommand):
                 return
 
             if not tasks:
+                window.run_command("hide_overlay")
                 message = "All packages up-to-date!"
                 console_write(message)
                 progress.finish(message)
@@ -46,11 +73,17 @@ class UpgradePackageCommand(sublime_aio.ApplicationCommand):
             0,
             sublime.QuickPanelItem(
                 trigger="Upgrade All Packages",
-                details="Use this command to install all available upgrades."
+                details="Use this command to install all available upgrades.",
             ),
         )
 
-        picked = await sublime_aio.active_window().show_quick_panel(items, sublime.KEEP_OPEN_ON_FOCUS_LOST)
+        if self.state == STATE_CANCELLED:
+            return
+
+        self.state = STATE_DISPLAY
+        window.run_command("hide_overlay")
+
+        picked = await window.show_quick_panel(items, sublime.KEEP_OPEN_ON_FOCUS_LOST)
         if picked < 0:
             return
 
